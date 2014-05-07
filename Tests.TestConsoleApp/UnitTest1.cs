a9fa055875603766c233ff9135cf93d4515c7504
jkeedhoo <smithajlk@hotmail.com>
2014-05-07 17:26:45 +0400

    change in method name
    name changed to ReturnsEmptyResult

--- Tests.TestConsoleApp/UnitTest1.cs
+++ Tests.TestConsoleApp/UnitTest1.cs
@@ -10,8 +10,9 @@ namespace Tests.TestConsoleApp
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ReturnsEmptyResult()
         {
+
         }
     }
 }
